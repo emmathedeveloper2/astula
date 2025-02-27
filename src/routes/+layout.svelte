952fb888a58303601f0233fe1664dfba4494bc4d
emmathedeveloper2 <emmadavid063@gmail.com>
2025-02-27 02:50:19 +0100

    Fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+  import checkForAppUpdates from '@lib/updater';
     import '../app.css'
 
     let { children } = $props()
+
+    checkForAppUpdates()
 </script>
 
 {@render children()}
